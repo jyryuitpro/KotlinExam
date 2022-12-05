b45c1e27ba5571a41ce138312b489e8bed0d4b84
jyr <jyr@wavers.co.kr>
2022-12-05 09:36:50 +0900

    066 오버라이딩(Overriding)

---
+++ src/main/kotlin/ex_overriding/Main.kt
@@ -0,0 +1,16 @@
+package ex_overriding
+
+open class AAA {
+    open fun func() = println("AAA")
+}
+
+class BBB : AAA() {
+    override fun func() {
+        super.func()
+        println("BBB")
+    }
+}
+fun main(args: Array<String>) {
+    AAA().func()
+    BBB().func()
+}
