31f526836d8fb340ee5dde619a58f6051455c945
jyr <jyr@wavers.co.kr>
2022-12-06 15:58:04 +0900

    124 열거 클래스 활용하기

---
+++ src/main/kotlin/ex_enum_class_practice/Main.kt
@@ -0,0 +1,21 @@
+package ex_enum_class_practice
+
+enum class Mode {
+    SELECTION,
+    PEN,
+    SHAPE,
+    ERASER
+}
+
+fun main(args: Array<String>) {
+    val shapeMode: Mode = Mode.SHAPE
+    println(shapeMode.name)
+    println(shapeMode.ordinal)
+
+    val modes: Array<Mode> = Mode.values()
+    for (mode: Mode in modes)
+        println(mode)
+
+    println(Mode.valueOf("PEN"))
+    println(Mode.valueOf("PEN").ordinal)
+}
