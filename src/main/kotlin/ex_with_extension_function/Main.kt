86ea4856cec26d592154830eb81d4e6f4c0d1d40
jyr <jyr@wavers.co.kr>
2022-12-07 14:51:24 +0900

    162 with 함수: 코드 중복 줄이기

---
+++ src/main/kotlin/ex_with_extension_function/Main.kt
@@ -0,0 +1,11 @@
+package ex_with_extension_function
+
+fun main(args: Array<String>) {
+    val a = 3
+    val b = 7
+
+    with(a * - b * b) {
+        println(this)
+        println(-this)
+    }
+}
