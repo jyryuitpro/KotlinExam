bed25b1404332f48cdac8a1f500df9ad09c97505
jyr <jyr@wavers.co.kr>
2022-12-07 10:41:58 +0900

    138 ListIterator 인터페이스 살펴보기

---
+++ src/main/kotlin/ex_list_iterator_interface/Main.kt
@@ -0,0 +1,16 @@
+package ex_list_iterator_interface
+
+private fun moveToNext(iter: ListIterator<Int>) {
+    print("${iter.hasPrevious()}, ")
+    print("${iter.previousIndex()}, ")
+    print("${iter.nextIndex()}, ")
+    println("${iter.next()}")
+}
+fun main(args: Array<String>) {
+
+    val iter: ListIterator<Int> = listOf<Int>(10, 20, 30).listIterator()
+
+    moveToNext(iter)
+    moveToNext(iter)
+    moveToNext(iter)
+}
